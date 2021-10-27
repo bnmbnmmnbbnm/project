ba66616e948c9dbdf197f07ccbfc6f8a90c22819
hieuchkxi <90381434+hieuchkxi@users.noreply.github.com>
2021-10-27 15:02:13 +0700

    commit

--- src/index.php
+++ src/index.php
@@ -10,32 +10,64 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Quản lý và sắp xếp lịch làm việc</title>
 </head>
 
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <img src="../images/logo.jpg"  alt="ảnh logo" style="width:100%;height:200px" >
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img src="../images/logo.jpg" alt="ảnh logo" style="width:100%;height:200px">
+                </div>
             </div>
         </div>
-    </div>
-</header>
-<body>
-    chúc mừng
-    <!-- Optional JavaScript; choose one of the two! -->
+        <div class="container">
+            <input type="email" class="form-control" id="floatingInput" placeholder="Tài Khoản">
+            <label for="floatingInput"></label>
+        </div>
+        <div class="container">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Mật khẩu">
+            <label for="floatingPassword"></label>
+        </div>
+        <div class="container">
+            <button name="bt" type="submit" class="btn btn-primary">Đăng nhập</button>
+        </div>
+    </header>
+    <form class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Họ tên</th>
+                    <th scope="col">Địa chỉ</th>
+                    <th scope="col">Tuổi</th>
+                    <th scope="col">Công việc</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Thời gian làm việc</th>
+                    <th scope="col">Sửa</th>
+                    <th scope="col">Xóa</th>
+                </tr>
+        </table>
+        <p align="center">
+            <button type="button"
+                style="background-color:yellow;margin-left:auto;margin-right:auto;display:block;margin-top:22%;margin-bottom:0%">
+                Thêm nhân viên</button>
+        </p>
+    </form>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <body>
+        <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-</body>
+    </body>
 
 </html>
